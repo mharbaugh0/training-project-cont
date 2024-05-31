515aea9b96310d70f14aed4a8bb8d9867a0adc97
mharbaugh0 <149205810+mharbaugh0@users.noreply.github.com>
2024-05-31 14:23:20 -0500

    Update app.vue with basic welcome message

--- Training-Project/app.vue
+++ Training-Project/app.vue
@@ -1,5 +1,20 @@
+@font-face {
+  font-family: 'FarAwayGalaxy';
+  src: url('/fonts/FarAwayGalaxy.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+  font-display: swap;
+} <!-- Don't forget to switch this out -->
+
+<style>
+h1, h3 {
+  font-family: 'FarAwayGalaxy', sans-serif;
+}
+</style>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+<div>
+  <h1 style="color:#143362"><center>Welcome</center></h1>
+  <h3 style="color:#42f59b"><center>Please enter login credentials</center></h3>
+</div>
 </template>
