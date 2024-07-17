f11780850014e6a6d972d9af6448cce22c9845f3
madelineharbaugh <mharbaugh@bioneos.com>
2024-07-17 16:01:36 -0500

    Logger comment

--- server/middleware/log.ts
+++ server/middleware/log.ts
@@ -2,4 +2,7 @@
 export default defineEventHandler((event) => {
     console.log('New request: ' + getRequestURL(event))
   })
+
+  //Nuxt 3.0 doesn't support winston or pino yet so requests are logged at handlers
+  //using Nuxt's existing structure
   
