6a3811947e49a3ef8042268fbeab62a702cb77b9
ziad hasabrabu <zhasabrabu@bioneos.com>
2024-08-01 17:38:18 -0500

    Created a password for MYSQL users to access db.

--- docker/initdb.sh
+++ docker/initdb.sh
@@ -4,7 +4,7 @@ set -e
 # Debugging: Print the environment variables
 echo "MYSQL_ROOT_PASSWORD: ${MYSQL_ROOT_PASSWORD}"
 echo "DATABASE_CONTAINER_USERNAME: ${MYSQL_USER}"
-# echo "DATABASE_CONTAINER_PASSWORD: ${DATABASE_CONTAINER_PASSWORD}"
+echo "MYSQL_PASSWORD: ${MYSQL_PASSWORD}"
 echo "MYSQL_DATABASE: ${MYSQL_DATABASE}"
 
 # Validate environment variables
@@ -18,6 +18,11 @@ if [ -z "${MYSQL_USER}" ]; then
     exit 1
 fi
 
+if [ -z "${MYSQL_PASSWORD}" ]; then
+    echo "Error: MYSQL_PASSWORD is not set"
+    exit 1
+fi
+
 if [ -z "${MYSQL_DATABASE}" ]; then
     echo "Error: MYSQL_DATABASE is not set"
     exit 1
@@ -25,7 +30,7 @@ fi
 
 mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<-EOSQL
     CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
-    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%';
+    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
     GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
     FLUSH PRIVILEGES;
 EOSQL
