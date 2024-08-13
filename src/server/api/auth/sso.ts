29e220aeaad197afe8574bd0b93120069b99526a
madelineharbaugh <mharbaugh@bioneos.com>
2024-08-13 11:16:06 -0500

    New auth file to handle logins via sso

---
+++ src/server/api/auth/sso.ts
@@ -0,0 +1,16 @@
+import { NuxtAuthHandler } from '#auth'
+import GithubProvider from 'next-auth/providers/github'
+
+const runtimeConfig = useRuntimeConfig()
+
+export default NuxtAuthHandler({
+  pages: {
+    signIn: '/login'
+  },
+  providers: [
+    GithubProvider.default({
+      clientId: runtimeConfig.public.GITHUB_CLIENT_ID,
+      clientSecret: runtimeConfig.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+})
